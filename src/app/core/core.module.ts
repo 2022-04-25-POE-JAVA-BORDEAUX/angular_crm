a54bf6624c84db11ae90889fecf97568400fc7ef
dnyuhm <dylanmeuriot@gmail.com>
2022-06-30 14:58:45 +0200

    Commit 2 : import CoreModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { NgModule } from '@angular/core';
+import { IconsModule } from '../icons/icons.module';
+import { LoginModule } from '../login/login.module';
+import { TemplatesModule } from '../templates/templates.module';
+import { UiModule } from '../ui/ui.module';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  exports: [LoginModule, IconsModule, TemplatesModule, UiModule],
 })
-export class CoreModule { }
+export class CoreModule {}
