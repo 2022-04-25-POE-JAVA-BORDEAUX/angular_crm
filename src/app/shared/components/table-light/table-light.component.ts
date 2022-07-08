226543ccc8ecbb1586dcbc87af152e22a10f4c3f
dnyuhm <dylanmeuriot@gmail.com>
2022-07-08 14:08:18 +0200

    Commit 26 : Projection d’un tableau dynamique

--- src/app/shared/components/table-light/table-light.component.ts
+++ src/app/shared/components/table-light/table-light.component.ts
@@ -1,16 +1,25 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Order } from 'src/app/core/models/order';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-table-light',
   templateUrl: './table-light.component.html',
   styleUrls: ['./table-light.component.scss'],
 })
-export class TableLightComponent implements OnInit {
+export class TableLightComponent implements OnInit, OnChanges {
   @Input() public headers!: string[];
-  @Input() public collection!: Order[];
 
-  constructor() {}
+  constructor() {
+    // console.log(this.headers);
+    // console.log(this.collection);
+  }
 
-  ngOnInit(): void {}
+  ngOnChanges(): void {
+    // console.log(this.headers);
+    // console.log(this.collection);
+  }
+
+  ngOnInit(): void {
+    // console.log(this.headers);
+    // console.log(this.collection);
+  }
 }
