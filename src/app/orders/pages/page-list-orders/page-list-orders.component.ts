f5a2cdba6902ac10f13845089ee0412f202d68d9
dnyuhm <dylanmeuriot@gmail.com>
2022-07-11 11:47:24 +0200

    Commit 28 : Pipe Angular &  changeState

--- src/app/orders/pages/page-list-orders/page-list-orders.component.ts
+++ src/app/orders/pages/page-list-orders/page-list-orders.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { StateOrder } from 'src/app/core/enums/state-order';
 import { Order } from 'src/app/core/models/order';
 import { OrdersService } from '../../services/orders.service';
 
@@ -12,16 +13,25 @@ export class PageListOrdersComponent implements OnInit {
   public title: string;
   public collection$: Observable<Order[]>;
   public headers: string[];
+  public states: string[];
 
   constructor(private ordersService: OrdersService) {
     this.title = 'list of orders';
-    this.headers = ['Type', 'désignation', 'nb jours', 'tjm', 'total', 'state'];
     this.collection$ = this.ordersService.collection$;
+    this.headers = ['Type', 'désignation', 'nb jours', 'tjm', 'total', 'state'];
+    this.states = Object.values(StateOrder);
   }
 
   ngOnInit(): void {}
 
   public changeTitle(): void {
     this.title = 'le titre a changé';
+  }
+
+  public changeState(item: Order, event: any) {
+    const state = event.target.value;
+    this.ordersService
+      .changeState(item, state)
+      .subscribe((data) => Object.assign(item, data));
   }
 }
--- src/app/orders/services/orders.service.ts
+++ src/app/orders/services/orders.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { StateOrder } from 'src/app/core/enums/state-order';
 import { Order } from 'src/app/core/models/order';
 import { environment } from 'src/environments/environment';
 
@@ -16,5 +17,15 @@ export class OrdersService {
     this.collection$ = this.httpClient.get<Order[]>(`${this.urlApi}/orders`);
 
     console.log(this.collection$);
+  }
+
+  public changeState(item: Order, state: StateOrder): Observable<Order> {
+    const obj = new Order(item);
+    obj.state = state;
+    return this.update(obj);
+  }
+
+  public update(item: Order): Observable<Order> {
+    return this.httpClient.put<Order>(`${this.urlApi}/orders/${item.id}`, item);
   }
 }
