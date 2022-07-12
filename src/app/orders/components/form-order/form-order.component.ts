eedd46360bc850aba9733d5b5fc4cc02dc051943
dnyuhm <dylanmeuriot@gmail.com>
2022-07-12 09:27:45 +0200

    Commit 32 : Formulaire & addOrder

---
+++ src/app/orders/components/form-order/form-order.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { StateOrder } from 'src/app/core/enums/state-order';
+import { Order } from 'src/app/core/models/order';
+
+@Component({
+  selector: 'app-form-order',
+  templateUrl: './form-order.component.html',
+  styleUrls: ['./form-order.component.scss'],
+})
+export class FormOrderComponent implements OnInit {
+  @Input() public init!: Order;
+  public states: string[];
+
+  constructor() {
+    this.states = Object.values(StateOrder);
+  }
+
+  ngOnInit(): void {}
+}
--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -6,12 +6,14 @@ import { OrdersRoutingModule } from './orders-routing.module';
 import { PageAddOrderComponent } from './pages/page-add-order/page-add-order.component';
 import { PageEditOrderComponent } from './pages/page-edit-order/page-edit-order.component';
 import { PageListOrdersComponent } from './pages/page-list-orders/page-list-orders.component';
+import { FormOrderComponent } from './components/form-order/form-order.component';
 
 @NgModule({
   declarations: [
     PageListOrdersComponent,
     PageAddOrderComponent,
     PageEditOrderComponent,
+    FormOrderComponent,
   ],
   imports: [CommonModule, OrdersRoutingModule, SharedModule],
 })
